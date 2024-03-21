f16e7bedbbadb1ba447660456f1de35223db6027
Guillaume Dorschner <guillaume.dorschner@icloud.com>
2024-03-21 18:00:47 +0100

    qsdqdq

---
+++ client/main.py
@@ -0,0 +1,19 @@
+# import requests
+
+SERVER_URL = "http://localhost:8000"
+
+
+def register(username, password):
+    pass
+
+
+def login(username, password):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
