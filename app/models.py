4fedba3eb6482a5c1d9438356464812b45471657
Guillaume Dorschner <guillaume.dorschner@icloud.com>
2024-03-23 18:42:15 +0100

    fix: types

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    client_public_key = db.Column(db.Text, nullable=False)
-    encrypted_envelope = db.Column(db.Text, nullable=False)
-    oprf_key = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    oprf_key = db.Column(db.LargeBinary, nullable=False)
+    encrypted_envelope = db.Column(db.LargeBinary, nullable=True)
+    client_public_key = db.Column(db.LargeBinary, nullable=True)
